b50de8340b24078f6223d16d3458d64a001a6c7d
Amy Rivera <amyrivera.ar@gmail.com>
2017-12-06 21:51:23 -0500

    added part03

--- app/src/main/java/nyc/amy_rivera_deveoloper/midtermassessment/ListActivity.java
+++ app/src/main/java/nyc/amy_rivera_deveoloper/midtermassessment/ListActivity.java
@@ -1,19 +1,66 @@
 package nyc.amy_rivera_deveoloper.midtermassessment;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class ListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListActivity extends Fragment {
 
     public static final String EXTRA_MESSAGE = "message";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_list, container, false);
+
+        Intent intent = getIntent();
+        String messageText= intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView) findViewById (R.id.message);
+        messageView.setText(messageText);
+    }
+
+    // RecyclerView user interface component
+    private RecyclerView recyclerView;
+
+    // Adapter for the RecyclerView
+    private RecyclerView.Adapter<ListViewHolder> adapter;
+
+    // LayoutManager for the RecyclerView
+    private RecyclerView.LayoutManager layoutManager;
+
+
+
+    public ListActivity() {
+        return List;
+
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+
 
 
     }
 }
+
+
---
+++ app/src/main/java/nyc/amy_rivera_deveoloper/midtermassessment/ListViewHolder.java
@@ -0,0 +1,14 @@
+package nyc.amy_rivera_deveoloper.midtermassessment;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by AmyRivera on 12/6/17.
+ */
+
+public class ListViewHolder extends RecyclerView.ViewHolder {
+    public ListViewHolder(View itemView) {
+        super(itemView);
+    }
+}
